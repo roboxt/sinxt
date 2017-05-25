43d0527dffb2170652f5383c151fb6e044699534
roboxt <robo.xt7aj@gmail.com>
2017-05-25 06:17:02 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,5 +78,5 @@ chmod_SinChi
 clear
  echo -n "" > sudo.lua
   read -p "What is your User Id? " -e input
- echo "sudo={111795059,$input}" >> sudo.lua
+ echo "sudo={186280031,$input}" >> sudo.lua
 logo2_SinChi
